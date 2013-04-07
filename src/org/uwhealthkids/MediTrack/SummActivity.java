0d1ad16f52ff3f05ef57c63961dcb3f1d11c73b2
Mike Wleklinski <mikewski333@gmail.com>
2013-04-07 16:57:00 -0500

    gui for summary pages

---
+++ src/org/uwhealthkids/MediTrack/SummActivity.java
@@ -0,0 +1,47 @@
+package org.uwhealthkids.MediTrack;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.CheckBox;
+
+public class SummActivity extends Activity {
+	
+	public static ArrayList<Integer> selections = new ArrayList<Integer>();
+	public final static String EXTRA_MESSAGE = "org.uwhealthkids.MediTrack.MESSAGE";
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_summ);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.sum, menu);
+		return true;
+	}
+	
+	public void buttonClicked(View view){
+		Intent toTFrame = new Intent(this, SummTFrameActivity.class);
+		//will only pass one selection
+		//TODO figure out how to pass multiple selections
+		if(selections.size() != 0){
+			CheckBox select = (CheckBox) findViewById(selections.get(0));
+			String message = (String) select.getText();
+			toTFrame.putExtra(EXTRA_MESSAGE, message);
+		}
+		startActivity(toTFrame);
+	}
+	
+	public void onOpSel(View view){
+		selections.add(view.getId());
+	}
+
+}
---
+++ src/org/uwhealthkids/MediTrack/SummTFrameActivity.java
@@ -0,0 +1,46 @@
+package org.uwhealthkids.MediTrack;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class SummTFrameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_summ_tframe);
+
+		//----------------------------------------------------------
+		//Used to test if information is passed correctly with intent
+	    // Get the message from the intent
+	    Intent intent = getIntent();
+	    String message = intent.getStringExtra(SummActivity.EXTRA_MESSAGE);
+
+	    // Create the text view
+	    if(message != null){
+	    	TextView textView = new TextView(this);
+	    	textView.setTextSize(40);
+	    	textView.setText(message);
+
+	    	// Set the text view as the activity layout
+	    	setContentView(textView);
+	    }
+	    //-----------------------------------------------------------
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.summ_tframe, menu);
+		return true;
+	}
+	
+	public void tFrameTapped(View view){
+		
+	}
+
+}
