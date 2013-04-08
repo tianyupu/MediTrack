93fa070a3d6a895cadab36b493542275c159f901
Mike Wleklinski <mikewski333@gmail.com>
2013-04-07 19:16:36 -0500

    summary page activity file

--- src/org/uwhealthkids/MediTrack/SummActivity.java
+++ src/org/uwhealthkids/MediTrack/SummActivity.java
@@ -21,6 +21,7 @@ public class SummActivity extends Activity {
 		setContentView(R.layout.activity_summ);
 	}
 
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
