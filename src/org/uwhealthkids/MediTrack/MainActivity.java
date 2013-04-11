50ac8b4d0c71b97891f0ab84d514dac9a48c43d5
Carrie Stienen <carrie.stienen@gmail.com>
2013-04-11 17:03:21 -0500

    Added some code to the main screen.

--- src/org/uwhealthkids/MediTrack/MainActivity.java
+++ src/org/uwhealthkids/MediTrack/MainActivity.java
@@ -3,13 +3,21 @@ package org.uwhealthkids.MediTrack;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	private EditText usernameEditText;
+	private EditText passwordEditText;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		usernameEditText = (EditText) findViewById(R.id.username_edittext);
+		passwordEditText = (EditText) findViewById(R.id.password_edittext);
 	}
 
 	@Override
@@ -19,4 +27,12 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public void onSignUpButtonClicked(View v) {
+		
+		
+	}
+	
+	public void onLoginButtonClicked(View v) {
+		
+	}
 }
