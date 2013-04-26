d103eb06497b36fbe73bc103d09a31e064d6fb58
Tian Pu <divine.infinity@gmail.com>
2013-04-26 14:53:32 -0500

    updated medication add to read names from the cloud and populate a dropdown

--- src/org/uwhealthkids/MediTrack/MedicationAddActivity.java
+++ src/org/uwhealthkids/MediTrack/MedicationAddActivity.java
@@ -1,17 +1,28 @@
 package org.uwhealthkids.MediTrack;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 public class MedicationAddActivity extends AddActivity {
+	public ArrayList<String> medicationNames = new ArrayList<String>();
 	public void onCreate(Bundle savedBundleInstance) {
 		super.onCreate(savedBundleInstance);
+		Parse.initialize(this, "Zx2IAp6TTPyM5UYRCr1Q4Q0GD0RyS0IDLzTm0aH0", "Dwj8peVWshOTpzos0Qae9yOBnhmZIMIxv4kJ6oTm");
 		
 		Intent i = getIntent();
 		int id = i.getExtras().getInt("charId");
@@ -19,19 +30,25 @@ public class MedicationAddActivity extends AddActivity {
 		setContentView(super.layouts[id]);
 		
 		Spinner spinner = (Spinner) findViewById(R.id.medication_spinner);
-		DBHelper dbHelper = new DBHelper(this);
-		
-		SQLiteDatabase db = dbHelper.getReadableDatabase();
-		Cursor cur = db.rawQuery("SELECT "+DBHelper.MEDICATION_COLUMN_NAME_NAME+"AS name FROM "+DBHelper.TABLE_NAME_MEDICATION, null);
-		ArrayList<String> medicationNames = new ArrayList<String>();
-		int index = cur.getColumnIndex("name");
-		if (cur.moveToFirst()) {
-			medicationNames.add(cur.getString(index));
-			while (cur.moveToNext()) {
-				medicationNames.add(cur.getString(index));
+		ParseQuery query = new ParseQuery("Medication");
+		query.setCachePolicy(ParseQuery.CachePolicy.CACHE_ELSE_NETWORK);
+		query.findInBackground(new FindCallback() {
+			@Override
+			public void done(List<ParseObject> medicationList, ParseException e) {
+				if (e == null) {
+					for (ParseObject obj : medicationList) {
+						String name = obj.getString("name");
+						if (!medicationNames.contains(name)) {
+							medicationNames.add(name);
+						}
+					}
+				} else {
+					Log.i("MedicationAddActivity", "error retrieving medications list");
+				}
 			}
-		}
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(spinner.getContext(), super.layouts[id], medicationNames);
+		});
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(spinner.getContext(), android.R.layout.simple_spinner_item, medicationNames);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinner.setAdapter(adapter);
 	}
 	
