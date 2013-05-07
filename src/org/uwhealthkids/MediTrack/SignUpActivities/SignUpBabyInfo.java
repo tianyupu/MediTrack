51eb8c4dc54aff1f61b2258ac7fde74c1c996c5b
Andrea Macan <macanandrea@gmail.com>
2013-05-06 22:40:20 -0500

    More sign-up baby stuff!

--- src/org/uwhealthkids/MediTrack/SignUpActivities/SignUpBabyInfo.java
+++ src/org/uwhealthkids/MediTrack/SignUpActivities/SignUpBabyInfo.java
@@ -1,6 +1,7 @@
 package org.uwhealthkids.MediTrack.SignUpActivities;
 
 
+import java.io.FileNotFoundException;
 import java.util.Calendar;
 
 import org.uwhealthkids.MediTrack.*;
@@ -12,25 +13,33 @@ import com.parse.ParseUser;
 import android.app.Activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.CalendarView;
+
 
 
 
 public class SignUpBabyInfo extends Activity{
+
+	TextView textTargetUri;
+	ImageView targetImage;
+	ImageButton buttonLoadImage;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.sign_up_baby_info);
 		Parse.initialize(this, "Zx2IAp6TTPyM5UYRCr1Q4Q0GD0RyS0IDLzTm0aH0", "Dwj8peVWshOTpzos0Qae9yOBnhmZIMIxv4kJ6oTm");
 	}
-
 
 
 	public void onSkipDocClicked(View v) {
@@ -55,12 +64,12 @@ public class SignUpBabyInfo extends Activity{
 		EditText babyname =  (EditText) findViewById(R.id.babyname);
 		RadioGroup gender = (RadioGroup) findViewById(R.id.genderRadioGroup);
 		RadioButton femaleButton = (RadioButton) findViewById(R.id.girl);
-		
+
 		// gets baby's birthday from date picker
 		DatePicker dob = (DatePicker) findViewById(R.id.dobPicker);
 		Calendar cal = Calendar.getInstance();
 		cal.set(dob.getYear(),dob.getMonth(),dob.getDayOfMonth());
-		
+
 
 		//checks which radio button was selected and changes the female boolean if it's a male
 		if(gender.getCheckedRadioButtonId() != femaleButton.getId()){
@@ -74,18 +83,46 @@ public class SignUpBabyInfo extends Activity{
 		baby.put("female", female);
 		baby.saveInBackground();
 
+		//makes the newly created baby the current baby
+		CustomApplication.getInstance().setCurrBaby(baby);
+
 		//creates a relationship object and puts the user and baby together
 		ParseUser user= ParseUser.getCurrentUser();
 		ParseObject Rel = new ParseObject("BabyUserRel");
 		Rel.put("baby", baby);
 		Rel.put("user", user);	
 		Rel.saveInBackground();
+
 	}
-	
 
 
+	public void button1Pressed(View v) { 
+		Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI); 
+		startActivityForResult(intent, 0);
+	}
 
-
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data); 
+		if (resultCode == RESULT_OK){
+			Uri targetUri = data.getData();
+			textTargetUri.setText(targetUri.toString());
+			Bitmap bitmapa; 
+			Bitmap bitmapb;
+			try {  
+				bitmapa = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+				bitmapb = Bitmap.createScaledBitmap (bitmapa,100,100,false);
+				buttonLoadImage.setImageBitmap(bitmapb); 
+			} catch (FileNotFoundException e) {  
+				// TODO Auto-generated catch block  
+				e.printStackTrace(); 
+			}
+		}
+	}
 }
+
+
+
+
 
 
